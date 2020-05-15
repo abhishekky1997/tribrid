d316369b2efedb5f4579ec1322f3fb513cd4170b
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-15 10:45:28 +0530

    added requirements

--- requirements.sh
+++ requirements.sh
@@ -10,6 +10,10 @@ sudo apt-get upgrade -y
 
 pip3 install -r requirements.txt
 
+# Downloading requirements
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1twcUs1sZSMwD3XHAtPtPfLivqwgIUSC2' -O 'encodings.pickle'
+wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
+unzip yolo.zip && rm yolo.zip
 
+# additional package for some rare dependency error on firebase
 python -m pip install pycrypto

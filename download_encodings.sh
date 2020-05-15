6642f206993965aa09c8a950512bdefeece9179c
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-15 10:50:37 +0530

    isolated downloads

---
+++ download_encodings.sh
@@ -0,0 +1,4 @@
+# Downloading requirements
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1twcUs1sZSMwD3XHAtPtPfLivqwgIUSC2' -O 'encodings.pickle'
+wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
+unzip yolo.zip && rm yolo.zip

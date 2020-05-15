65dd13ef42ad9fa8ad72d88d0879982c2794be7f
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-15 12:49:08 +0530

    put while loop under  exception handling

--- tribrid_final.py
+++ tribrid_final.py
@@ -61,144 +61,146 @@ activity_count = 0
 
 # TODO: Main Loop
 fps = FPS().start()
-while(True):
-# -----------------------------------------------------------------------------------------------
-# TODO: Activity Monitoring
-# -----------------------------------------------------------------------------------------------
-	_, frame = cap.read()                           # capture image
-	# get length & width of image
-	rows, cols, _ = np.shape(frame)
-	# compute pythogorean distance
-	dist = distMap(frame1, frame)
-	frame1 = frame2                                 # reassign x[-2] frame
-	frame2 = frame                                  # reassign x[-1] frame
-	mod = cv2.GaussianBlur(dist, (9, 9), 0)          # Apply gaussian smoothing
-	_, thresh = cv2.threshold(mod, 100, 255, 0)     # Thresholding
-	# calculate std deviation test
-	_, stDev = cv2.meanStdDev(mod)
+try:
+	while(True):
+	# -----------------------------------------------------------------------------------------------
+	# TODO: Activity Monitoring
+	# -----------------------------------------------------------------------------------------------
+		_, frame = cap.read()                           # capture image
+		# get length & width of image
+		rows, cols, _ = np.shape(frame)
+		# compute pythogorean distance
+		dist = distMap(frame1, frame)
+		frame1 = frame2                                 # reassign x[-2] frame
+		frame2 = frame                                  # reassign x[-1] frame
+		mod = cv2.GaussianBlur(dist, (9, 9), 0)          # Apply gaussian smoothing
+		_, thresh = cv2.threshold(mod, 100, 255, 0)     # Thresholding
+		# calculate std deviation test
+		_, stDev = cv2.meanStdDev(mod)
 
-	if stDev > sdThresh: activity_count += 1          # computing activity intensity
-	# push to motion detection data to cloud
-	if(time.time()-time1 >= 5):
-			time1 = time.time()
-			nowtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-			firebase.patch('/Motion Detection/', {nowtime: activity_count})
-			activity_count = 0
-			print("activity detected")
+		if stDev > sdThresh: activity_count += 1          # computing activity intensity
+		# push to motion detection data to cloud
+		if(time.time()-time1 >= 5):
+				time1 = time.time()
+				nowtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+				firebase.patch('/Motion Detection/', {nowtime: activity_count})
+				print(f"activity Intensity - {activity_count}")
+				activity_count = 0
 
-# -----------------------------------------------------------------------------------------------
-# TODO: Facial Recognition
-# -----------------------------------------------------------------------------------------------
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # grayscale image
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    # rgb image
+	# -----------------------------------------------------------------------------------------------
+	# TODO: Facial Recognition
+	# -----------------------------------------------------------------------------------------------
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # grayscale image
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    # rgb image
 
-	dets = detector(rgb, 1)
-	boxes = [(d.left(), d.top(), d.right(), d.bottom())\
-				for i, d in enumerate(dets)]        # get tuple of box coordinates
-	encodings = face_recognition.face_encodings(rgb, boxes) # encode those faces from rgb
+		dets = detector(rgb, 1)
+		boxes = [(d.left(), d.top(), d.right(), d.bottom())\
+					for i, d in enumerate(dets)]        # get tuple of box coordinates
+		encodings = face_recognition.face_encodings(rgb, boxes) # encode those faces from rgb
 
-	names = []
-	text = "Unoccupied"
-	# Loop over facial embeddings and check if faces match
-	for encoding in encodings:
-		matches = face_recognition.compare_faces(data["encodings"], encoding)
-		name = "Unknown"
-		if True in matches:
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+		names = []
+		text = "Unoccupied"
+		# Loop over facial embeddings and check if faces match
+		for encoding in encodings:
+			matches = face_recognition.compare_faces(data["encodings"], encoding)
+			name = "Unknown"
+			if True in matches:
+				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+				counts = {}
 
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
-			name = max(counts, key=counts.get)
-			print(f"{name} was detected in room R088")
-			text = 'Occupied'
+				for i in matchedIdxs:
+					name = data["names"][i]
+					counts[name] = counts.get(name, 0) + 1
+				name = max(counts, key=counts.get)
+				text = 'Occupied'
 
-		# patching data to firebase console
-		x = datetime.datetime.now().strftime("%H:%M:%S")
-		y = datetime.datetime.now().strftime("%Y-%m-%d")
-		firebase.patch('/Monitoring/'+name+'/'+y+'/', {x: "At camera "+room})
-		names.append(name)
-	print(*names, 'are found in ', room)
+			# patching data to firebase console
+			x = datetime.datetime.now().strftime("%H:%M:%S")
+			y = datetime.datetime.now().strftime("%Y-%m-%d")
+			for i in names:
+				firebase.patch('/Monitoring/'+i+'/'+y+'/', {x: "At camera "+room})
+			names.append(name)
+		print(*names, 'are found in', room)
 
-# -----------------------------------------------------------------------------------------------
-# TODO: object detection
-# -----------------------------------------------------------------------------------------------
-	blob = cv2.dnn.blobFromImage(
-		frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-	net.setInput(blob)
-	layerOutputs = net.forward(ln)
+	# -----------------------------------------------------------------------------------------------
+	# TODO: object detection
+	# -----------------------------------------------------------------------------------------------
+		blob = cv2.dnn.blobFromImage(
+			rgb, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+		net.setInput(blob)
+		layerOutputs = net.forward(ln)
 
-	# initialize lists of detected bounding boxes, confidences, and class IDs, respectively
-	boxes = []
-	confidences = []
-	classIDs = []
+		# initialize lists of detected bounding boxes, confidences, and class IDs, respectively
+		boxes = []
+		confidences = []
+		classIDs = []
 
-	# loop over each of the layer outputs
-	for output in layerOutputs:
-		# loop over each of the detections
-		for detection in output:
-			# extract class ID and confidence by using score for each class
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+		# loop over each of the layer outputs
+		for output in layerOutputs:
+			# loop over each of the detections
+			for detection in output:
+				# extract class ID and confidence by using score for each class
+				scores = detection[5:]
+				classID = np.argmax(scores)
+				confidence = scores[classID]
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > confidence_score:
-				# YOLO returns center coords, width and height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
-				# derive top left corner for NMS
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
-				# Update all
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+				# filter out weak predictions by ensuring the detected
+				# probability is greater than the minimum probability
+				if confidence > confidence_score:
+					# YOLO returns center coords, width and height
+					box = detection[0:4] * np.array([W, H, W, H])
+					(centerX, centerY, width, height) = box.astype("int")
+					# derive top left corner for NMS
+					x = int(centerX - (width / 2))
+					y = int(centerY - (height / 2))
+					# Update all
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					classIDs.append(classID)
 
-	# apply NMS
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence_score, threshold)
-	if len(idxs) > 0:  # if detected
-		# loop over detected objects and push to cloud
-		for i in idxs.flatten():
-			detected_object = LABELS[classIDs[i]]
-			nowtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-			firebase.patch("/Object Detection/",{nowtime:f"{detected_object} detected at camera {room}"})
+		# apply NMS
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence_score, threshold)
+		if len(idxs) > 0:  # if detected
+			# loop over detected objects and push to cloud
+			for i in idxs.flatten():
+				detected_object = LABELS[classIDs[i]]
+				nowtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+				firebase.patch("/Object Detection/",{nowtime:f"{detected_object} detected at camera {room}"})
+				print(f"{detected_object} found in room {room}")
 
-# -----------------------------------------------------------------------------------------------
-# TODO: room occupacy status
-# -----------------------------------------------------------------------------------------------
-	today=date.today().strftime("%m:%d:%Y")
-	if text == 'Occupied':
-		t = time.localtime()
-		current_time = time.strftime("%H:%M:%S", t)
-		firebase.patch(f'/Room Occupied/{room}/'+today,{current_time:text})
-		result=firebase.get(f'/Room Occupied/{room}/'+today,'Occupied')
-		if(result==None):
-			firebase.patch(f'/Room Occupied/{room}/'+today,{'Occupied':1})
-		else:
+	# -----------------------------------------------------------------------------------------------
+	# TODO: room occupacy status
+	# -----------------------------------------------------------------------------------------------
+		today=date.today().strftime("%m:%d:%Y")
+		if text == 'Occupied':
+			t = time.localtime()
+			current_time = time.strftime("%H:%M:%S", t)
+			firebase.patch(f'/Room Occupied/{room}/'+today,{current_time:text})
 			result=firebase.get(f'/Room Occupied/{room}/'+today,'Occupied')
-			result+=1
-			firebase.patch(f'/Room Occupied/{room}/'+today,{'Occupied':result})
-	else:
-		t = time.localtime()
-		current_time = time.strftime("%H:%M:%S", t)
-		firebase.patch(f'/Room Occupied/{room}/'+today,{current_time:text})
-		result=firebase.get(f'/Room Occupied/{room}/'+today,'Unoccupied')
-		if(result==None):
-			firebase.patch(f'/Room Occupied/{room}/'+today,{'Unoccupied':1})
+			if(result==None):
+				firebase.patch(f'/Room Occupied/{room}/'+today,{'Occupied':1})
+			else:
+				result=firebase.get(f'/Room Occupied/{room}/'+today,'Occupied')
+				result+=1
+				firebase.patch(f'/Room Occupied/{room}/'+today,{'Occupied':result})
 		else:
+			t = time.localtime()
+			current_time = time.strftime("%H:%M:%S", t)
+			firebase.patch(f'/Room Occupied/{room}/'+today,{current_time:text})
 			result=firebase.get(f'/Room Occupied/{room}/'+today,'Unoccupied')
-			result+=1
-			firebase.patch(f'/Room Occupied/{room}/'+today,{'Unoccupied':result})
-	
-	if cv2.waitKey(1) & 0xFF == 27: break       # break if esc is pressed
-	fps.update()
-# -----------------------------------------------------------------------------------------------
-# TODO: End loop
-# -----------------------------------------------------------------------------------------------
-
+			if(result==None):
+				firebase.patch(f'/Room Occupied/{room}/'+today,{'Unoccupied':1})
+			else:
+				result=firebase.get(f'/Room Occupied/{room}/'+today,'Unoccupied')
+				result+=1
+				firebase.patch(f'/Room Occupied/{room}/'+today,{'Unoccupied':result})
+		
+		if cv2.waitKey(1) & 0xFF == 27: break       # break if esc is pressed
+		fps.update()
+	# -----------------------------------------------------------------------------------------------
+	# TODO: End loop
+	# -----------------------------------------------------------------------------------------------
+except KeyboardInterrupt: pass
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))

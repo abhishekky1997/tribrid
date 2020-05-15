03d7eae21c63bb9132be4947b9179ba962314043
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-15 11:51:31 +0530

    pushed pickle file to dropbox

--- download_encodings.sh
+++ download_encodings.sh
@@ -1,4 +1,4 @@
 # Downloading requirements
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1twcUs1sZSMwD3XHAtPtPfLivqwgIUSC2' -O 'encodings.pickle'
+wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
 wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
 unzip yolo.zip && rm yolo.zip

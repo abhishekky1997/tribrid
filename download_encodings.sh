06f4dc53dc5872681bfa78eaef7a4b804372b01d
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-23 07:41:28 +0530

    one more change-3

--- download_encodings.sh
+++ download_encodings.sh
@@ -3,7 +3,8 @@ if [[! -f "encodings.pickle"]]
 then
     wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
 fi
-if [[! -f yolo/obj.cfg]] && [[! -f yolo/obj.names]] && [[! -f yolo/obj.weights]]
+dir = yolo/
+if [[! -d dir]]
 then
     wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
     unzip yolo.zip && rm yolo.zip

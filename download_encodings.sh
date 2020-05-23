cda2fb31f77293602955eb705db77efcb9b7aa69
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-23 07:49:17 +0530

    one more change-4

--- download_encodings.sh
+++ download_encodings.sh
@@ -1,11 +1,9 @@
 # Downloading requirements
-if [[! -f "encodings.pickle"]]
-then
+if [[! -f "encodings.pickle"]]; then
     wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
 fi
-dir = "yolo/"
-if [! -d "$dir"]
-then
+dir = "~/tribrid/yolo/"
+if [! -d "$dir"]; then
     wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
     unzip yolo.zip && rm yolo.zip
 else

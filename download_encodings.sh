9dc12a8e0f3ba3ed770bbfaaca13963db278dcb2
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-23 07:36:57 +0530

    modified download_encodings.sh

--- download_encodings.sh
+++ download_encodings.sh
@@ -1,4 +1,10 @@
 # Downloading requirements
-wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
-wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
+if [[! -f "encodings.pickle"]]
+then
+    wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
+fi
+if [[! -f yolo/obj.cfg]] && [[! -f yolo/obj.names]] && [[! -f yolo/obj.weights]]
+then
+    wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
+fi
 unzip yolo.zip && rm yolo.zip

d1b93c495e9f8685cf79ec12eafe9638bb3d871d
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-23 07:38:31 +0530

    one more change

--- download_encodings.sh
+++ download_encodings.sh
@@ -6,5 +6,7 @@ fi
 if [[! -f yolo/obj.cfg]] && [[! -f yolo/obj.names]] && [[! -f yolo/obj.weights]]
 then
     wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
+    unzip yolo.zip && rm yolo.zip
+else
+    rm -rf yolo/
 fi
-unzip yolo.zip && rm yolo.zip

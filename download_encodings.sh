2882b02be62cfe8ea03a0f2643a67df48291da02
sangeeth98 <sangeethkumarvm@gmail.com>
2020-05-23 07:47:06 +0530

    modified download_encodings.sh

--- download_encodings.sh
+++ download_encodings.sh
@@ -4,7 +4,7 @@ then
     wget https://www.dropbox.com/s/zkg7py03ijej315/encodings.pickle
 fi
 dir = "yolo/"
-if [! -d dir]
+if [! -d "$dir"]
 then
     wget https://www.dropbox.com/s/15oxq46b0qzflkw/yolo.zip
     unzip yolo.zip && rm yolo.zip
